664e6ccd5dc23d6a31601a225baf7f803dad6782
Jac22 <jacopo.prudentino@gmail.com>
2022-11-21 16:23:49 +0100

    Milestone 0

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const ourTeam = [];
+
+const wayne = {
+    "nome" : "Wayne",
+    "cognome" : "Barnett",
+    "role" : "Founder and CEO",
+    "image" : "wayne-barnett-founder-ceo.jpg"
+}
+
+ourTeam.push(wayne);
+
+const angelaC = {
+    "nome" : "Angela",
+    "cognome" : "Caroll",
+    "role" : "Chief Editor",
+    "image" : "angela-caroll-chief-editor.jpg"
+}
+
+ourTeam.push(angelaC);
+
+const walter = {
+    "nome" : "Walter",
+    "cognome" : "Gordon",
+    "role" : "Office Manager",
+    "image" : "walter-gordon-office-manager.jpg"
+}
+
+ourTeam.push(walter);
+
+const angelaL = {
+    "nome" : "Angela",
+    "cognome" : "Lopez",
+    "role" : "Social Media Manager",
+    "image" : "angela-lopez-social-media-manager.jpg"
+}
+
+ourTeam.push(angelaL);
+
+const scott = {
+    "nome" : "Scott",
+    "cognome" : "Estrada",
+    "role" : "Developer",
+    "image" : "scott-estrada-developer.jpg"
+}
+
+ourTeam.push(scott);
+
+const barbara = {
+    "nome" : "Barbara",
+    "cognome" : "Ramos",
+    "role" : "Graphic Designer",
+    "image" : "barbara-ramos-graphic-designer.jpg"
+}
+
+ourTeam.push(barbara);
+
+console.log(ourTeam);
