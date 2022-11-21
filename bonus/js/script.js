83b2cbfea709d8590f0e4b2c9cefcddbbbcfbaec
Jac22 <jacopo.prudentino@gmail.com>
2022-11-21 16:56:10 +0100

    Completato bonus 1

---
+++ bonus/js/script.js
@@ -0,0 +1,74 @@
+const container = document.getElementById("container");
+const ourTeam = [];
+
+const wayne = {
+    "name" : "Wayne",
+    "surname" : "Barnett",
+    "role" : "Founder and CEO",
+    "image" : "wayne-barnett-founder-ceo.jpg"
+}
+
+ourTeam.push(wayne);
+
+const angelaC = {
+    "name" : "Angela",
+    "surname" : "Caroll",
+    "role" : "Chief Editor",
+    "image" : "angela-caroll-chief-editor.jpg"
+}
+
+ourTeam.push(angelaC);
+
+const walter = {
+    "name" : "Walter",
+    "surname" : "Gordon",
+    "role" : "Office Manager",
+    "image" : "walter-gordon-office-manager.jpg"
+}
+
+ourTeam.push(walter);
+
+const angelaL = {
+    "name" : "Angela",
+    "surname" : "Lopez",
+    "role" : "Social Media Manager",
+    "image" : "angela-lopez-social-media-manager.jpg"
+}
+
+ourTeam.push(angelaL);
+
+const scott = {
+    "name" : "Scott",
+    "surname" : "Estrada",
+    "role" : "Developer",
+    "image" : "scott-estrada-developer.jpg"
+}
+
+ourTeam.push(scott);
+
+const barbara = {
+    "name" : "Barbara",
+    "surname" : "Ramos",
+    "role" : "Graphic Designer",
+    "image" : "barbara-ramos-graphic-designer.jpg"
+}
+
+ourTeam.push(barbara);
+
+for(let i=0; i<ourTeam.length; i++){
+    console.log(ourTeam[i]);
+    const newElement = document.createElement("div");
+    for(let key in ourTeam[i]){
+        console.log(key, ":", ourTeam[i][key]);
+
+        if(key=="image"){
+            console.log("pippo")
+            newElement.innerHTML += `<img src="img/${ourTeam[i][key]}">`
+        }
+        else{
+            newElement.innerHTML += `<div>${key}: ${ourTeam[i][key]}</div>`
+        }
+    }
+    newElement.style.width=100/ourTeam.length + "%";
+    container.appendChild(newElement);
+}
